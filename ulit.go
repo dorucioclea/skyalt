78e0499635496758241a8ed4e6a078ea630a7bc0
milansuk <milansuk86@gmail.com>
2023-09-10 00:10:11 +0200

    Added: Copy file

--- ulit.go
+++ ulit.go
@@ -18,6 +18,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"math"
 	"os"
 	"os/exec"
@@ -517,6 +518,31 @@ func OsFileRemove(path string) error {
 }
 func OsFileRename(path string, newPath string) error {
 	return os.Rename(path, newPath)
+}
+
+func OsFileCopy(src, dst string) error {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return fmt.Errorf("%s is not a regular file", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+	_, err = io.Copy(destination, source)
+	return err
 }
 
 const g_special = "ěščřžťďňĚŠČŘŽŤĎŇáíýéóúůÁÍÝÉÓÚŮÄäÖöÜüẞß"
